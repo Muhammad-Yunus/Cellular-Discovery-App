51847f8222354aba3767e4e08af7a2f9cb256f31
Muhammad Yunus <muhammadyunusiot@gmail.com>
2026-08-12 11:59:12 +0700

    fix(route-map): update drone icon to correct Lucide SVG path

--- app/components/RouteMap.vue
+++ app/components/RouteMap.vue
@@ -105,17 +105,14 @@ function esc(html: string): string {
     // Different icon based on status: drone for IDLE, arrow for MOVING
     let iconSvg = ''
     if (status === 'IDLE') {
-      // Drone/quadcopter icon
+      // Drone/quadcopter icon (Lucide style)
       iconSvg = `<svg class="drone-icon-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" style="width:18px;height:18px;">
-        <circle cx="12" cy="12" r="2"/>
-        <path d="M12 2v4"/>
-        <path d="M12 18v4"/>
-        <path d="M2 12h4"/>
-        <path d="M18 12h4"/>
-        <path d="M4.93 4.93l2.83 2.83"/>
-        <path d="M16.24 16.24l2.83 2.83"/>
-        <path d="M4.93 19.07l2.83-2.83"/>
-        <path d="M16.24 7.76l2.83-2.83"/>
+        <path d="M10 10 7 7"/><path d="m10 14-3 3"/><path d="m14 10 3-3"/><path d="m14 14 3 3"/>
+        <path d="M14.205 4.139a4 4 0 1 1 5.439 5.863"/>
+        <path d="M19.637 14a4 4 0 1 1-5.432 5.868"/>
+        <path d="M4.367 10a4 4 0 1 1 5.438-5.862"/>
+        <path d="M9.795 19.862a4 4 0 1 1-5.429-5.873"/>
+        <rect x="10" y="8" width="4" height="8" rx="1"/>
       </svg>`
     } else {
       // Arrow/direction icon for MOVING and other statuses
